cba931a0244867789a711217a5f0701d2aaacb41
Max <sydneymax@gmail.com>
2019-01-20 18:02:31 +1100

    Create base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchCountriesComponent } from './components/search-countries/search-countries.component';
+import { SearchHistoryComponent } from './components/search-history/search-history.component';
+import { SearchInputComponent } from './components/search-input/search-input.component';
+import { SearchResultComponent } from './components/search-result/search-result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchCountriesComponent,
+    SearchHistoryComponent,
+    SearchInputComponent,
+    SearchResultComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/search-countries/search-countries.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-countries',
+  templateUrl: './search-countries.component.html',
+  styleUrls: ['./search-countries.component.css']
+})
+export class SearchCountriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
